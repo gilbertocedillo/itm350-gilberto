94226c6ebaa4f36282ea7f9f9aff7e951a267e80
Gilberto Cedillo <ced23003@byui.edu>
2024-10-30 16:34:40 -0600

    test!

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    collectCoverage: true,
     coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80
+      './src/*.js': {
+        lines: 80
       }
-    }
-  };
+    },
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+    collectCoverageFrom: ['src/**/*.js']
+  }
   
